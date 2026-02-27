e18511b8da46f7294e7fd292e62da24bd6811a08
Adrian <adrianafnandika@gmail.com>
2026-02-28 03:09:17 +0700

    add shp resolver

--- main.go
+++ main.go
@@ -5,6 +5,21 @@ import (
 	"net/http"
 )
 
+func corsAll(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	log.Println("registering handlers")
 	mux := http.NewServeMux()
@@ -13,7 +28,7 @@ func main() {
 	mux.HandleFunc("/", notFoundHandler)
 
 	log.Println("HTTP server listening on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", corsAll(mux)); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
 }
